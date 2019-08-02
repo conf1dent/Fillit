a594c0af464d39a2d9b8d289542e0b7694784ae6
Hlib Vasyliev <hvasylie@e1z1r6p21.42.us.org>
2019-08-01 17:59:18 -0700

    fillit

--- move_left_top.c
+++ move_left_top.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   move.c                                             :+:      :+:    :+:   */
+/*   move_left_top.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hvasylie <hvasylie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/30 19:44:56 by hvasylie          #+#    #+#             */
-/*   Updated: 2019/08/01 17:36:29 by hvasylie         ###   ########.fr       */
+/*   Created: 2019/07/31 14:21:22 by hvasylie          #+#    #+#             */
+/*   Updated: 2019/08/01 17:58:55 by hvasylie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static int		get_width(t_tetr *tetrimino)
 
 	len = ft_strlen(tetrimino->str) - 1;
 	i = 0; //we will need to input size through the struct (4 in basic case)
-	while (i < tetrimino->size)
+	while(i < tetrimino->size)
 	{
 		size = tetrimino->size;
 		while (size != 0)
@@ -35,11 +35,11 @@ static int		get_width(t_tetr *tetrimino)
 	return (0);
 }
 
-static int		check_right(t_tetr *tetrimino)
+static int		check_left(t_tetr *tetrimino)
 {
 	int i;
 
-	i = tetrimino->size - 1; //we will need to input size through the struct (4 in basic case)
+	i = tetrimino->size + 1 - tetrimino->size; //we will need to input size through the struct (4 in basic case)
 	while (tetrimino->str[i])
 	{
 		if (tetrimino->str[i] == '#')
@@ -49,45 +49,47 @@ static int		check_right(t_tetr *tetrimino)
 	return (0);
 }
 
-static int		check_botom(t_tetr *tetrimino)
+static int		check_top(t_tetr *tetrimino)
 {
 	int len;
 	int size;
 
 	size = tetrimino->size; // we'll need to pass input size through the struct (4 in basic case)
-	len = ft_strlen(tetrimino->str) - 1;
-	while (size != 0)
+	len = 0;
+	while(len < size) // because size starts from 1, len from 0
 	{
 		if (tetrimino->str[len] == '#')
 			return (1);
-		size--;
-		len--;
+		len++;
 	}
 	return (0);
 }
 
-int				move(t_tetr *tetrimino)
+// nado sdelat
+int				move_left(t_tetr *tetrimino)
 {
 	int		width;
 	int		len;
+	int		i;
 
-	len = ft_strlen(tetrimino->str) - 1;
+	i = 0;
+	len = ft_strlen(tetrimino->str);
 	width = get_width(tetrimino);
-	if (check_right(tetrimino) && check_botom(tetrimino))
+	if (check_left(tetrimino) && check_top(tetrimino))
 		return (1);
-	if (check_right(tetrimino))
-		while (len >= 0)
+	if (check_left(tetrimino))
+		while (i < len)
 		{
-			if (tetrimino->str[len] == '#')
-				ft_swap(tetrimino->str[len], tetrimino->str[len + width]);
-			len--;
+			if (tetrimino->str[i] == '#')
+				ft_swap(tetrimino->str[i], tetrimino->str[i - width]);
+			i++;
 		}
 	else
-		while (len >= 0)
+		while (len > i)
 		{
-			if (tetrimino->str[len] == '#')
-				ft_swap(tetrimino->str[len], tetrimino->str[len + 1]);
-			len--;
+			if (tetrimino->str[i] == '#')
+				ft_swap(tetrimino->str[i], tetrimino->str[i - 1]);
+			i++;
 		}
 	return (0);
 }
